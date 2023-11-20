19e9b504e9c27a4e5702cdac7320bf1224e85274
Steve Cho <iamstevecho@gmail.com>
2023-11-19 20:56:48 -0500

    restructured spec tests

--- spec/calc_spec.rb
+++ spec/calc_spec.rb
@@ -4,23 +4,25 @@ RSpec.describe Calc do
   number_words = %w[zero one two three four five six seven eight nine]
   operation_words = %w[plus minus times divided_by]
 
-  context 'digit methods' do
-    number_words.each do |word|
-      it "responds to the #{word} method" do
-        expect(Calc.new).to respond_to(word)
+  context 'method availability' do
+    context 'digit methods' do
+      number_words.each do |word|
+        it "responds to the #{word} method" do
+          expect(Calc.new).to respond_to(word)
+        end
+      end
+    end
+
+    context 'operation methods' do
+      operation_words.each do |operator|
+        it "responds to the #{operator} method" do
+          expect(Calc.new).to respond_to(operator)
+        end
       end
     end
   end
 
-  context 'operation methods' do
-    operation_words.each do |operator|
-      it "responds to the #{operator} method" do
-        expect(Calc.new).to respond_to(operator)
-      end
-    end
-  end
-
-  context 'calculations' do
+  context 'arithmetic operations' do
     it 'calculates one plus two' do
       expect(Calc.new.one.plus.two).to eq(3)
     end
@@ -36,15 +38,9 @@ RSpec.describe Calc do
     it 'calculates nine divided by three' do
       expect(Calc.new.nine.divided_by.three).to eq(3)
     end
+  end
 
-    it 'raises an error for undefined methods' do
-      expect { Calc.new.ten }.to raise_error(NoMethodError)
-    end
-
-    it 'returns an integer for non-integer division results' do
-      expect(Calc.new.one.divided_by.two).to be_an(Integer)
-    end
-
+  context 'special cases' do
     it 'calculates zero times any number' do
       expect(Calc.new.zero.times.five).to eq(0)
     end
@@ -54,11 +50,25 @@ RSpec.describe Calc do
     end
 
     it 'handles large numbers' do
-      expect(Calc.new.five.times.nine).to eq(45)
+      expect(Calc.new.nine.times.nine).to eq(81)
     end
 
     it 'handles negative results correctly' do
       expect(Calc.new.one.minus.two).to eq(-1)
+    end
+
+    it 'correctly calculates when chaining multiple operations with digits in between' do
+      expect(Calc.new.one.plus.minus.two).to eq(-1)
+    end
+  end
+
+  context 'error handling' do
+    it 'raises an error for undefined methods' do
+      expect { Calc.new.ten }.to raise_error(NoMethodError)
+    end
+
+    it 'ensures results of division are integers' do
+      expect(Calc.new.one.divided_by.two).to be_an(Integer)
     end
 
     it 'raises an error when dividing by zero' do
